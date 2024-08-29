25efff3067238713b1a17714f95708c71d61b1d3
airtonbjunior <airtonbjunior@gmail.com>
2024-08-29 09:52:08 -0300

    Controle de ponto

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const diaSemana = document.getElementById("dia-semana");
+const diaMesAno = document.getElementById("dia-mes-ano");
+const horaMinSeg = document.getElementById("hora-min-seg");
+
+diaSemana.textContent = "asdfasfdsafsaf";
+
+
+console.log(diaSemana.textContent);
