18f72395d7e52748c23f16f692bf45bc6dada85e
airtonbjunior <airtonbjunior@gmail.com>
2024-08-29 10:47:42 -0300

    final aula 29-8

--- js/index.js
+++ js/index.js
@@ -2,7 +2,23 @@ const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
-diaSemana.textContent = "asdfasfdsafsaf";
 
+// Todo conjunto numérico (exceto ano) deve ter 2 dígitos (adicionar 0 se for menor q 10)
+// Retornar dia da semana por extenso (em pt-br)
 
-console.log(diaSemana.textContent);
+function diaSemana() {
+    //retornar dia da semana
+}
+
+function dataCompleta() {
+    const date = new Date();
+    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+}
+
+function horaCompleta() {
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+diaMesAno.textContent = dataCompleta();
+horaMinSeg.textContent = horaCompleta();
