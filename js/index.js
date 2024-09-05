6ab2e51aecf46d1f543f61bf2d3351d1d5840e98
airtonbjunior <airtonbjunior@gmail.com>
2024-09-05 08:59:12 -0300

    aula 5-9

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
+const arrayDayWeek = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sabado"]
 
 
 // Todo conjunto numérico (exceto ano) deve ter 2 dígitos (adicionar 0 se for menor q 10)
 // Retornar dia da semana por extenso (em pt-br)
 
-function diaSemana() {
+function daySemana() {
     //retornar dia da semana
+    const date = new Date();
+    return arrayDayWeek[date.getDay()];
 }
 
 function dataCompleta() {
@@ -20,5 +23,11 @@ function horaCompleta() {
     return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
+function atualizaHora() {
+    horaMinSeg.textContent = horaCompleta();
+}
+
+setInterval(atualizaHora, 1000);
+
+diaSemana.textContent = daySemana();
 diaMesAno.textContent = dataCompleta();
-horaMinSeg.textContent = horaCompleta();
