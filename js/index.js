588162de2dff0383441d45a88789971b410138c2
airtonbjunior <airtonbjunior@gmail.com>
2024-09-19 09:14:41 -0300

    aula 19-09

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 const arrayDayWeek = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sabado"]
 
-
 const dialogPonto = document.getElementById("dialog-ponto");
 
 
@@ -17,6 +16,13 @@ navigator.geolocation.getCurrentPosition((position) => {
 });
 
 
+let proxPonto = {
+    "entrada": "intervalo",
+    "intervalo": "volta-intervalo",
+    "volta-intervalo": "saida",
+    "saida": "entrada"
+}
+
 // TO-DO:
 // apresentar para o usuário a data e hora atualizados
 // atualizar a data todos os dias 00:00
@@ -25,9 +31,12 @@ const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
 btnRegistrarPonto.addEventListener("click", () => {
     // TO-DO:
     // 1 - recuperar o select por meio de id ("select-tipos-ponto")
+    let dialogSelect = document.getElementById("select-tipos-ponto");
+    
     // 2 - recuperar o tipo do ultimo ponto que está salvo no localstorage
     // 2.1 - salvamos o tipo na chave "tipoUltimoPonto"
     // 2.2 - conseguimos recuperar um valor do localstorage com o getItem(chave)
+    let ultimoPonto = localStorage.getItem("tipoUltimoPonto");
     
     // 3 - Fazer uma condicional e atribuir o valor do select conforme tabela
     // tipo ultimo ponto  |  valor select
@@ -35,9 +44,10 @@ btnRegistrarPonto.addEventListener("click", () => {
     // intervalo          |  volta-intervalo
     // volta-intervalo    |  saida
     // saida              |  entrada
+    // let proximoPonto = proxPonto[/*tipo do último ponto*/];
 
     // 3.1 para setar o valor de um select, usamos o atributo value
-    
+    dialogSelect.value = proxPonto[ultimoPonto];
     
     dialogPonto.showModal();
 });
@@ -75,7 +85,7 @@ btnDialogRegistrarPonto.addEventListener("click", () => {
     // Exemplo: usuário registrou "entrada", determinar que o select apresente "intervalo" como valor padrão
 
     console.log(ponto);
-    
+    dialogPonto.close();
 });
 
 function daySemana() {
