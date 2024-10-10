1760a64b88f736e01d3ec3a9aad2fc09fae1db52
airtonbjunior <airtonbjunior@gmail.com>
2024-10-10 09:38:38 -0300

    aula 10-10 intervalo

--- js/relatorio.js
+++ js/relatorio.js
@@ -1,11 +1,34 @@
-/* 
+function criarRelatorio() {
 
-1. recuperar os registros do localstorage (getItem)
-1.1 índice "registro"
+    const containerRegistros = document.getElementById("container-registros");
 
-2. iterar sobre os registros
-2.1 para cada registro, criar um elemento na página
-2.2 Tipo | hora | obs? | anexo? | editar | excluir
-2.3 agrupar registros por data
+    let registros = JSON.parse(localStorage.getItem("registro"));
+    registros.forEach(registro => {
+        console.log(registro);
+        
+        const divRegistro = document.createElement("div");
 
-*/
+        let hora = registro.hora;
+
+        // para cara registro, temos
+        // hora: registro.hora (já está na variável hora)
+        // data: registro.data
+        // tipo: registro.tipo
+
+        // Escreva no seguinte formato: TIPO | DATA | HORA | 
+        divRegistro.innerHTML = "<p>Olá, Mundo! " + hora + "</p>";
+
+        // Adicionar botões
+        containerRegistros.appendChild(divRegistro);
+    });
+
+    /* 
+    2. iterar sobre os registros
+    2.1 para cada registro, criar um elemento na página
+    2.2 Tipo | hora | obs? | anexo? | editar | excluir
+    2.3 agrupar registros por data
+
+    */
+}
+
+criarRelatorio();
